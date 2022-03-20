b863683ebaf6e05d1fe6d1bbea9c10a1cda0b9b6
LasithEranga <lasitheranga1@gmail.com>
2022-03-20 23:29:52 +0530

    node application to get and filter data

--- Form1.cs
+++ Form1.cs
@@ -1,40 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using System.Threading.Tasks;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.IO;
 using System.Net;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
 
 namespace PowerCutSchedule
 {
     public partial class PowerCutSchedule : Form
     {
-        public  PowerCutSchedule()
+        public PowerCutSchedule()
         {
+            
             InitializeComponent();
-            WebClient client = new WebClient();
+            //WebClient client = new WebClient();
 
             // Add a user agent header in case the
             // requested URI contains a query.
 
-            client.Headers.Add("user-agent", "PostmanRuntime/7.29.0");
-            client.Headers.Add("Cookie", ".AspNetCore.Antiforgery.ThOcTlhnrMo=CfDJ8DRug-ybtbxAjw2fB8wdOCNFI98Jv_Jwb7v7LVad84zXCqypz2plALDzqEPkKUB8OMjniOAj80fiZA0fy-Ld9uzzmApq5TmqWYyJHib5ajckBJ1_GPh4euVbSDvSODNzdAaP8N4CxqWvuxNj5ftu9Zc");
-            client.Headers.Add("RequestVerificationToken", "CfDJ8DRug-ybtbxAjw2fB8wdOCOVCtuvvGJqobQz-1nkU3ndefl5UgXmsO5OGq3FR30MbhT0cdB_NrbdhSfqrNaA-LbqTJRd2-fbDStpFxQagsrt0p2VHa4no5UjFwv6T-JwogQGhA4zMSpducoqFz8GVP8");
-            String data = client.UploadString("https://cebcare.ceb.lk/Incognito/GetLoadSheddingEvents","");
-            
-            Console.WriteLine(data);
+            //client.Headers.Add("user-agent", "PostmanRuntime/7.29.0");
+            //client.Headers.Add("Cookie", ".AspNetCore.Antiforgery.ThOcTlhnrMo=CfDJ8DRug-ybtbxAjw2fB8wdOCNFI98Jv_Jwb7v7LVad84zXCqypz2plALDzqEPkKUB8OMjniOAj80fiZA0fy-Ld9uzzmApq5TmqWYyJHib5ajckBJ1_GPh4euVbSDvSODNzdAaP8N4CxqWvuxNj5ftu9Zc");
+            //client.Headers.Add("RequestVerificationToken", "CfDJ8DRug-ybtbxAjw2fB8wdOCOVCtuvvGJqobQz-1nkU3ndefl5UgXmsO5OGq3FR30MbhT0cdB_NrbdhSfqrNaA-LbqTJRd2-fbDStpFxQagsrt0p2VHa4no5UjFwv6T-JwogQGhA4zMSpducoqFz8GVP8");
+            //String data = client.UploadString("https://cebcare.ceb.lk/Incognito/GetLoadSheddingEvents", "");
+            // data = data.Substring(1, data.Length - 2);
+            var time = new List<string>();
+            foreach (string line in System.IO.File.ReadLines(@"./test.txt"))
+            {
+                time.Add(line);
+            }
+
+            DateTime thisDay = DateTime.Today;
+            date.Text = thisDay.ToString("d");
+
+            day.Text = time[0] +" To "+ time[1];
+            night.Text = time[2] + " To " + time[3];
 
 
         }
-
-       
-
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
+using System.Diagnostics;
 
 namespace PowerCutSchedule
 {
@@ -14,6 +15,21 @@ namespace PowerCutSchedule
         [STAThread]
         static void Main()
         {
+
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine("node getData.js");
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+            Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new PowerCutSchedule());
